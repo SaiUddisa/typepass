d4424307d2413e73146e8f95bea2e5c2bf5b9e54
Sai Uddisa <saiuddisa18@gmail.com>
2024-11-17 09:27:43 +0530

    made changes to make this project compatible in mobile env

--- glow.js
+++ glow.js
@@ -1,9 +1,20 @@
 let key;
 document.addEventListener("keydown", (event) => {
   key = event.key;
-
   if (key === " ") {
     const keyPressed = document.querySelector(`.space`);
+    keyPressed.classList.add("glow");
+    setTimeout(() => {
+      keyPressed.classList.remove("glow");
+    }, 300);
+  }else if(key == 'Shift'){
+    const keyPressed = document.querySelector(`.shift`);
+    keyPressed.classList.add("glow");
+    setTimeout(() => {
+      keyPressed.classList.remove("glow");
+    }, 300);
+  }else if(key == 'Backspace'){
+    const keyPressed = document.querySelector(`.Backspace`);
     keyPressed.classList.add("glow");
     setTimeout(() => {
       keyPressed.classList.remove("glow");
@@ -15,3 +26,12 @@ document.addEventListener("keydown", (event) => {
     keyPressed.classList.remove("glow");
   }, 300);
 });
+
+function keyglow(key){
+  const keyPressed = document.getElementById(key);
+  keyPressed.classList.add("glow");
+  setTimeout(() => {
+    keyPressed.classList.remove("glow");
+  }, 300);
+
+}
--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 async function getQuote() {
-  // const respones = await fetch(
-  //   "https://official-joke-api.appspot.com/random_joke"
-  // );
-  // quoteObj = await respones.json();
-  // let quote = quoteObj.setup + " " + quoteObj.punchline;
-  let quote = "Nothing goes as planned in this accursed world!! This quote is said by Madara uchiha also known as the ghost of uchiha";
+  const respones = await fetch(
+    "https://official-joke-api.appspot.com/random_joke"
+  );
+  quoteObj = await respones.json();
+  let quote = quoteObj.setup + " " + quoteObj.punchline;
+  // let quote = "Nothing goes as planned in this accursed world!!";
   if (quote.length > 2000) {
     getQuote();
   }
@@ -20,6 +20,11 @@ async function getQuote() {
   paragraphTag.innerHTML = displayText;
   let index = 0;
   let typedKey;
+  let shiftToggle = false;
+  
+
+  let keyboard = { '0': ')', '1': '!', '2': '@', '3': '#', '4': '$', '5': '%', '6': '^', '7': '&', '8': '*', '9': '(', 'a': 'A', 'b': 'B', 'c': 'C', 'd': 'D', 'e': 'E', 'f': 'F', 'g': 'G', 'h': 'H', 'i': 'I', 'j': 'J', 'k': 'K', 'l': 'L', 'm': 'M', 'n': 'N', 'o': 'O', 'p': 'P', 'q': 'Q', 'r': 'R', 's': 'S', 't': 'T', 'u': 'U', 'v': 'V', 'w': 'W', 'x': 'X', 'y': 'Y', 'z': 'Z' };
+
   let mistakes = 0;
   document.addEventListener("keydown", (event) => {
     typedKey = event.key;
@@ -71,6 +76,108 @@ async function getQuote() {
 
       paragraphTag.innerHTML = `<h2> Accuracy : ${accuracy}% </h2>`;
     }
+
   });
+
+
+
+  document.getElementById('Shift').addEventListener('click', () => {
+    shiftToggle = !shiftToggle;
+    let shift = document.getElementById('Shift');
+    if (shiftToggle) {
+      shift.classList.add("glow");
+      for (i in keyboard) {
+        document.getElementById(i).innerHTML = keyboard[i];
+      }
+    } else {
+      shift.classList.remove("glow");
+      for (i in keyboard) {
+        document.getElementById(i).innerHTML = i;
+      }
+    }
+
+  });
+  onload
+
+
+  const listItems = document.querySelectorAll('li');
+
+
+
+  listItems.forEach(listItem => {
+
+    listItem.addEventListener('click', function () {
+      if (shiftToggle) {
+        typedKey = keyboard[listItem.id];
+      } else {
+        typedKey = listItem.id;
+      }
+       
+    if(listItem.id =='Shift'){
+      typedKey  = listItem.id;
+    }
+    if(listItem.id == 'Backspace'){
+      typedKey  = listItem.id;
+    }
+
+      if (typedKey === quote[index]) {
+        document.querySelector(`.span${index}`).classList.add("success");
+        index++;
+        //for coorecting mistakenly typed words
+      } else if (typedKey == "Backspace") {
+        if (index > 0) {
+          index--;
+          if (mistakes > 0) {
+            mistakes--;
+          }
+        }
+        document.querySelector(`.span${index}`).classList.remove("success");
+        document.querySelector(`.span${index}`).classList.remove("failure");
+      } else if (
+        typedKey == "Shift" ||
+        typedKey == "Alt" ||
+        typedKey == "Control" ||
+        typedKey == "AltGraph" ||
+        typedKey == "Tab" ||
+        typedKey == "ArrowUp" ||
+        typedKey == "ArrowDown" ||
+        typedKey == "ArrowLeft" ||
+        typedKey == "ArrowRight" ||
+        typedKey == "CapsLock" ||
+        typedKey == "NumLock" ||
+        typedKey == "PageUp" ||
+        typedKey == "PageDown"
+      ) {
+        if (index > 0) { index = index; }
+      } else {
+        document.querySelector(`.span${index}`).classList.add("failure");
+        mistakes++;
+        index++;
+      }
+
+      // if (index >= quote.length) {
+      //   // if (
+      //   //   confirm(`You made  mistakes ${mistakes} times.Press OK to try again`)
+      //   // ) {
+      //   //   location.reload();
+      //   // }
+
+      //   let correct = quote.length - mistakes;
+      //   let accuracy = Math.round((correct / quote.length) * 100);
+
+      //   paragraphTag.innerHTML = `<h2> Accuracy : ${accuracy}% </h2>`;
+      // }
+
+
+    });
+
+  });
+
+
+
+
+
+
+
 }
 getQuote();
